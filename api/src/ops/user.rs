21ae4e92c7f0cc191ec57c4e41210bb13d1d30cf
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2023-01-07 22:15:07 -0300

    Ajustes

--- api/src/ops/user.rs
+++ api/src/ops/user.rs
@@ -1,21 +1,22 @@
-use crate::models::user::*;
 use diesel::prelude::*;
+use crate::models::product::*;
+
 
 pub type DbError = Box<dyn std::error::Error + Send + Sync>;
 
-pub fn find_all(conn: &MysqlConnection) -> Result<Vec<User>, DbError> {
-    use crate::schema::users::dsl::*;
+pub fn find_all(conn: &MysqlConnection) -> Result<Vec<Product>, DbError> {
+    use crate::schema::produto::dsl::*;
 
-    let res = users
-        .load::<User>(conn)?;
+    let res = produto
+        .load::<Product>(conn)?;
 
     Ok(res)
 }
 
-pub fn find_by_id(_id: u64, conn: &MysqlConnection) -> Result<Option<User>, DbError> {
-    use crate::schema::users::dsl::*;
+pub fn find_by_id(_id: u64, conn: &MysqlConnection) -> Result<Option<Product>, DbError> {
+    use crate::schema::produto::dsl::*;
 
-    let res = users
+    let res = produto
         .find(_id)
         .first(conn)
         .optional()?;
@@ -23,26 +24,26 @@ pub fn find_by_id(_id: u64, conn: &MysqlConnection) -> Result<Option<User>, DbEr
     Ok(res)
 }
 
-pub fn add_user(user: UserPayload, conn: &MysqlConnection) -> Result<usize, DbError> {
-    use crate::schema::users::dsl::*;
+pub fn add_user(user: ProductPayload, conn: &MysqlConnection) -> Result<usize, DbError> {
+    use crate::schema::produto::dsl::*;
 
-    let new_user = NewUser {
+    let new_user = NewProduct {
         name: user.name,
         email: user.email,
         created_at: chrono::Local::now().naive_local(),
     };
 
-    let res = diesel::insert_into(users)
+    let res = diesel::insert_into(produto)
         .values(new_user)
         .execute(conn)?;
 
     Ok(res)
 }
 
-pub fn update_user(_id: u64, user: UserPayload, conn: &MysqlConnection) -> Result<usize, DbError> {
-    use crate::schema::users::dsl::*;
+pub fn update_user(_id: u64, user: ProductPayload, conn: &MysqlConnection) -> Result<usize, DbError> {
+    use crate::schema::produto::dsl::*;
 
-    let res = diesel::update(users.find(_id))
+    let res = diesel::update(produto.find(_id))
         .set(user)
         .execute(conn)?;
 
@@ -50,9 +51,9 @@ pub fn update_user(_id: u64, user: UserPayload, conn: &MysqlConnection) -> Resul
 }
 
 pub fn delete_user(_id: u64, conn: &MysqlConnection) -> Result<usize, DbError> {
-    use crate::schema::users::dsl::*;
+    use crate::schema::produto::dsl::*;
 
-    let res = diesel::delete(users.find(_id))
+    let res = diesel::delete(produto.find(_id))
         .execute(conn)?;
 
     Ok(res)
